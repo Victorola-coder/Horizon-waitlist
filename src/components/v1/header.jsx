88e5f3498861479be269722b89b2b7b6b04684b4
Victor Olatunji VickyJay <victoluolatunji@gmail.com>
2023-08-08 14:31:35 +0100

    installled react-router-dom

---
+++ src/components/v1/header.jsx
@@ -0,0 +1,11 @@
+export default function header() {
+  return (
+    <div>
+      <header className={``}>
+        <nav className={`flex items-center justify-between`}>
+          <h2 >Horizon</h2>
+        </nav>
+      </header>
+    </div>
+  );
+}
